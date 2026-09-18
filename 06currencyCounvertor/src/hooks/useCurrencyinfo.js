feac91cb61b2a88dfd7de35d62377af77c29dd70
TruptiRamanuj <ramanujtrupti2005@gmail.com>
2026-09-18 19:17:54 +0530

    06 debuging add

--- 06currencyCounvertor/src/hooks/useCurrencyinfo.js
+++ 06currencyCounvertor/src/hooks/useCurrencyinfo.js
@@ -1,14 +1,23 @@
 import { useEffect, useState } from "react";
 
 function useCurrencyinfo(curr){
-    const [data,setData]=({})
-useEffect(()=>{
-    fetch('https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/usd.json')
-    .then((res)=>res.json)
-    .then((res)=>setData(res[curr]))
+    const [data,setData]=useState({});
+
+
+    useEffect(()=>{
+    fetch( `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${curr}.json`)
+    .then((res)=>res.json())
+    .then((res)=>{
+        setData(res[curr]);
+        console.log(res[curr]);
+    })
+    .catch((err)=>{
+        console.log("Error:",err);
+    });
+
+    },[curr]);
+    
     console.log(data);
-},[curr])
-console.log(data);
-return data
+    return data;
 }
 export default useCurrencyinfo;
--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -1,10 +1,50 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import Layout from './Layout'
+import Home from './components/Home/Home'
+import About from './components/About/About'
+import Contact from './components/Contact/contact'
+import User from './components/User/User'
+import Github from './components/Github/Github'
+
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: "",
+//         element: <Home />
+//       },
+//       {
+//         path: "about",
+//         element: <About />
+//       },
+//        {
+//         path: "contact",
+//         element: <Contact />
+//       }
+//     ]
+//   }
+// ]) 
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />} />
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
+      <Route path='user/:userid' element={<User />} />
+      <Route path='github' element={<Github />} />
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+<React.StrictMode>
+  <RouterProvider router={router}/>
+</React.StrictMode>,
 )
